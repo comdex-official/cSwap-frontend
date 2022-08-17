aa2681b4c292ab6e5602b44fb4e855816ce5c7c7
srinu391 <srinu@comdex.sg>
2022-08-17 17:02:27 +0530

    Update taking environment variables.

--- src/config/network.js
+++ src/config/network.js
@@ -1,8 +1,8 @@
 export const comdex = {
   chainId: process.env.REACT_APP_CHAIN_ID,
   chainName: process.env.REACT_APP_CHAIN_NAME,
-  rpc: "https://vtest-rpc.comdex.one",
-  rest: "https://vtest-rest.comdex.one",
+  rpc: process.env.REACT_APP_RPC,
+  rest: process.env.REACT_APP_REST,
   explorerUrlToTx: process.env.REACT_APP_EXPLORER_URL_TO_TX,
   coinDenom: "CMDX",
   coinMinimalDenom: "ucmdx",
@@ -24,8 +24,9 @@ export const harbor = {
 };
 
 export const ibcDenoms = {
-  uatom: process.env.REACT_APP_ATOM_IBC_DENOM,
-  uosmo: process.env.REACT_APP_OSMO_IBC_DENOM,
+  uatom: "ibc/27394FB092D2ECCD56123C74F36E4C1F926001CEADA9CA97EA622B25F41E5EB2",
+  uxprt: "ibc/1DA0D68FA3369C706C7FD8F2EDF0F35BD32EB9F02887B050622AE034E93523D0",
+  uosmo: "ibc/ED07A3391A112B175915CD8FAF43A2DA8E4790EDE12566649D0C2F97716B8518",
 };
 
 export const tokenCoinGeckoIds = [
