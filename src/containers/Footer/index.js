14eef6d3271fcb614dcc3ce2aec9357359994328
asif <asif.ansari7774@gmail.com>
2022-08-25 16:59:09 +0530

    social link updated

--- src/containers/Footer/index.js
+++ src/containers/Footer/index.js
@@ -7,31 +7,8 @@ const Footer = () => {
     <div className="footer">
       <div className="social-icons">
         <a
-          aria-label="Discord"
-          target="_blank"
-          rel="noreferrer"
-          href="https://bit.ly/ComdexOfficialDiscord"
-        >
-          <SvgIcon name="discord" viewbox="0 0 29.539 22.155" />
-        </a>
-        <a
-          aria-label="Github"
-          target="_blank"
-          rel="noreferrer"
-          href="https://github.com/comdex-official"
-        >
-          <SvgIcon name="github" viewbox="0 0 22.154 21.607" />
-        </a>
-        <a
-          aria-label="Telegram"
-          target="_blank"
-          rel="noreferrer"
-          href="https://t.me/cSwap_DEX"
-        >
-          <SvgIcon name="telegram" viewbox="0 0 24.635 20.66" />
-        </a>
-        <a
           aria-label="Twitter"
+          title="Twitter"
           target="_blank"
           rel="noreferrer"
           href="https://twitter.com/cSwap_DEX"
@@ -39,12 +16,85 @@ const Footer = () => {
           <SvgIcon name="twitter" viewbox="0 0 25.617 20.825" />
         </a>
         <a
+          aria-label="Telegram"
+          title="Telegram"
+          target="_blank"
+          rel="noreferrer"
+          href="https://t.me/cSwap_DEX"
+        >
+          <SvgIcon name="telegram" viewbox="0 0 24.635 20.66" />
+        </a>
+        <a
+          aria-label="Discord"
+          title="Discord"
+          target="_blank"
+          rel="noreferrer"
+          href="https://bit.ly/ComdexOfficialDiscord"
+        >
+          <SvgIcon name="discord" viewbox="0 0 29.539 22.155" />
+        </a>
+        <a
           aria-label="Medium"
+          title="Medium"
           target="_blank"
           rel="noreferrer"
           href="https://medium.com/@cSwap_DEX"
         >
           <SvgIcon name="medium" viewbox="0 0 25.825 20.66" />
+        </a>
+        <a
+          aria-label="Forum"
+          title="Forum"
+          target="_blank"
+          rel="noreferrer"
+          href="https://forum.comdex.one/"
+        >
+          <SvgIcon name="forum" viewbox="0 0 20 20" />
+        </a>
+        <a
+          aria-label="Linkedin"
+          title="Linkedin"
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.linkedin.com/showcase/cswap-dex/"
+        >
+          <SvgIcon name="linkedin" viewbox="0 0 19.975 19.975" />
+        </a>
+        <a
+          aria-label="Reddit"
+          title="Reddit"
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.reddit.com/r/ComdexOne/"
+        >
+          <SvgIcon name="reddit" viewbox="0 0 20 20" />
+        </a>
+        <a
+          aria-label="Instagram"
+          title="Instagram"
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.instagram.com/comdexone/"
+        >
+          <SvgIcon name="instagram" viewbox="0 0 19.975 19.97" />
+        </a>
+        <a
+          aria-label="Github"
+          title="Github"
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/comdex-official"
+        >
+          <SvgIcon name="github" viewbox="0 0 22.154 21.607" />
+        </a>
+        <a
+          aria-label="Newsletter"
+          title="Newsletter"
+          target="_blank"
+          rel="noreferrer"
+          href="https://comdexnewsletter.com/subscribe"
+        >
+          <SvgIcon name="newsletter" viewbox="0 0 23.915 19.132" />
         </a>
       </div>
     </div>
--- src/utils/SvgSpriteLoader.js
+++ src/utils/SvgSpriteLoader.js
@@ -7,7 +7,7 @@ const SvgSprite = (props) => {
   const [isErrored, setIsErrored] = useState(false);
 
   useEffect(() => {
-    const svgVersion = "1.2.6";
+    const svgVersion = "1.2.8";
     const isLocalStorage =
       "localStorage" in window && window.localStorage !== null;
     let data;
