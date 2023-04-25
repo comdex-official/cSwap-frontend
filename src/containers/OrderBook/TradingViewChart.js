ed2ad68ac3e3f4392f1723326c30a70122c3a12d
srinu391 <srinu@comdex.sg>
2023-04-25 17:29:46 +0530

    Update theme.

--- src/containers/OrderBook/TradingViewChart.js
+++ src/containers/OrderBook/TradingViewChart.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from "react";
-import { widget } from '../../charting_library';
+import { widget } from "../../charting_library";
 import Datafeed from "./datafeed";
 
 const TradingViewChart = ({ symbol = "Bitfinex:BTC/USD" }) => {
@@ -13,13 +13,30 @@ const TradingViewChart = ({ symbol = "Bitfinex:BTC/USD" }) => {
       datafeed: Datafeed,
       library_path: "/charting_library/",
       locale: "en",
-      theme: "Light",
+      theme: "dark",
       autosize: true,
-      toolbar_bg: "#F1F3F6",
       enable_publishing: false,
       allow_symbol_change: true,
       show_popup_button: true,
       save_image: false,
+      disabled_features: [
+        "volume_force_overlay",
+        "header_compare",
+        "header_interval_dialog_button",
+        "show_interval_dialog_on_key_press",
+        "header_symbol_search",
+        "header_saveload",
+      ],
+      enabled_features: [
+        "move_logo_to_main_pane",
+        "hide_last_na_study_output",
+        "clear_bars_on_series_error",
+        "dont_show_boolean_study_arguments",
+        "narrow_chart_enabled",
+        "side_toolbar_in_fullscreen_mode",
+        "save_chart_properties_to_local_storage",
+        "use_localstorage_for_settings",
+      ],
       studies_overrides: {},
       overrides: {},
     });
