b05f7b7648921dff1b34454578fb52ba60eaeae8
Priyanshu1710 <guptapriyanshu1710@gmail.com>
2023-05-03 05:24:41 +0530

    Added govern page and highcharts.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,22 @@ import type { AppProps } from 'next/app';
 import dynamic from 'next/dynamic';
 import '../styles/global.scss';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import { MantineProvider } from '@mantine/core';
 
 const Layout = dynamic(() => import('@/modules/layout/Layout'));
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme: 'light',
+        }}
+      >
+        <Layout>
+        <Component {...pageProps} />
+        </Layout>
+      </MantineProvider>
   );
 }
