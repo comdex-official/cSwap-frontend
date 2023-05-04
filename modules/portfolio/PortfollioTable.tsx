d587d921036c32374d91a358edb2252241f5ca15
svarcoder <charvai1997@gmail.com>
2023-05-04 19:20:23 +0530

    feat: added liquidity modal, Liquidity table fixed, added portfolio tabled

--- modules/portfolio/PortfollioTable.tsx
+++ modules/portfolio/PortfollioTable.tsx
@@ -1,7 +1,8 @@
 import dynamic from 'next/dynamic';
 import styles from '../assets/Assets.module.scss';
+import styles1 from '../portfolio/Portfolio.module.scss';
 import { NextImage } from '@/shared/image/NextImage';
-import { ArrowRight, CMDS } from '@/shared/image';
+import { ATOM, ArrowRight, CMDS } from '@/shared/image';
 
 const Table = dynamic(() => import('@/shared/components/table/Table'));
 
@@ -242,14 +243,120 @@ const PortfolioTable = ({ theme, active }: AssetTableProps) => {
     },
   ];
 
+  const LiquidityCOLUMNS = [
+    {
+      Header: 'Asset Pair',
+      accessor: 'Asset',
+      Cell: ({ row, value }: any) => (
+        <div
+          className={`${styles1.portfolio__table__asset} ${
+            theme === 'dark' ? styles1.dark : styles1.light
+          }`}
+        >
+          <div
+            className={`${styles1.farmCard__element__left__logo__wrap} ${
+              theme === 'dark' ? styles1.dark : styles1.light
+            }`}
+          >
+            <div
+              className={`${styles1.farmCard__element__left__logo} ${
+                styles1.first
+              } ${theme === 'dark' ? styles1.dark : styles1.light}`}
+            >
+              <div
+                className={`${styles1.farmCard__element__left__logo__main} ${
+                  theme === 'dark' ? styles1.dark : styles1.light
+                }`}
+              >
+                <NextImage src={row.original.Image1} alt="" />
+              </div>
+            </div>
+            <div
+              className={`${styles1.farmCard__element__left__logo} ${
+                styles1.last
+              } ${theme === 'dark' ? styles1.dark : styles1.light}`}
+            >
+              <div
+                className={`${styles1.farmCard__element__left__logo__main} ${
+                  theme === 'dark' ? styles1.dark : styles1.light
+                }`}
+              >
+                <NextImage src={row.original.Image2} alt="" />
+              </div>
+            </div>
+          </div>
+
+          <div
+            className={`${styles1.farmCard__element__left__title} ${
+              styles1.tableActive
+            } ${theme === 'dark' ? styles1.dark : styles1.light}`}
+          >
+            {value}
+          </div>
+        </div>
+      ),
+    },
+    {
+      Header: 'APR',
+      accessor: 'APR',
+      Cell: ({ row, value }: any) => (
+        <div
+          className={`${styles1.portfolio__table__apr} ${
+            theme === 'dark' ? styles1.dark : styles1.light
+          }`}
+        >
+          <NextImage src={row.original.Image3} alt="Logo" />
+          {value}
+        </div>
+      ),
+    },
+    {
+      Header: 'My Liquidity',
+      accessor: 'MyLiquidity',
+      Cell: ({ value }: any) => (
+        <div
+          className={`${styles1.portfolio__table__liquidity} ${
+            theme === 'dark' ? styles1.dark : styles1.light
+          }`}
+        >
+          ${value}
+        </div>
+      ),
+    },
+    {
+      Header: 'Action',
+      accessor: 'Action',
+      Cell: ({ value }: any) => (
+        <div
+          className={`${styles1.farmCard__buttonWrap} ${
+            theme === 'dark' ? styles1.dark : styles1.light
+          }`}
+        >
+          <button>{'Manage'}</button>
+        </div>
+      ),
+    },
+  ];
+
+  const LiquidityData = [
+    {
+      Asset: 'CMDX-CANTO',
+      Image1: CMDS,
+      Image2: ATOM,
+      Image3: CMDS,
+      APR: 'External - 149%',
+      MyLiquidity: 1.53,
+    },
+  ];
+
   return (
     <>
       {active === 'Assets' ? (
         <Table columns={COLUMNS} data={DATA} />
       ) : active === 'Liquidity' ? (
-        <Table columns={COLUMNS} data={[]} />
+        <Table columns={LiquidityCOLUMNS} data={LiquidityData} />
       ) : active === 'History' ? (
-        <Table columns={COLUMNS} data={[]} />
+        <Table columns={LiquidityCOLUMNS} data={[]} />
       ) : (
         ''
       )}
