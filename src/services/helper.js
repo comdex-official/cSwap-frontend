25188c6579899766cc6c3880efc7988feb4f4b01
Priyanshu1710 <guptapriyanshu1710@gmail.com>
2023-06-24 05:36:13 +0530

    Fixed stride ibc deposit issue.

--- src/services/helper.js
+++ src/services/helper.js
@@ -215,3 +215,30 @@ export const aminoSignIBCTx = (config, transaction, callback) => {
       });
   })();
 };
+
+export const aminoDirectSignIBCTx = async (transaction, address, config, callback) => {
+  const [offlineSigner, accounts] = await KeplrWallet(config.chainId);
+
+  SigningStargateClient.connectWithSigner(config.rpc, offlineSigner, {
+    registry: myRegistry,
+    aminoTypes: aminoTypes,
+  })
+    .then((client) => {
+      client
+        .signAndBroadcast(
+          address,
+          [transaction.message],
+          transaction.fee,
+          transaction.memo
+        )
+        .then((result) => {
+          callback(null, result);
+        })
+        .catch((error) => {
+          callback(error?.message);
+        });
+    })
+    .catch((error) => {
+      callback(error && error.message);
+    });
+}
